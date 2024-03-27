3ba54ff6bef0478085e22b937390c565a02bbaa7
melpd <melanie.desroches@uconn.edu>
2024-03-27 10:49:35 -0400

    added scatterplot

--- F1_stats/F1_Drivers.py
+++ F1_stats/F1_Drivers.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 df = pd.read_csv('C:/Users/mpdes/OneDrive/Desktop/Stat4188/F1_stats/F1DriversDataset.csv')
 print(df.head())
 active_drivers = df[df['Active']]
-print(len(active_drivers))
+print(active_drivers)
+print(active_drivers['Nationality'])
+plt.scatter(df['Points_Per_Entry'], df['Win_Rate'])
+plt.show()
+#Danni Ric needs to be changed to active instead of Lawson unless RB decides to change things up again
